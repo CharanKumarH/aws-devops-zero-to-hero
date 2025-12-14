0de6db9b6148aa5825b6b04c26a79846e029fc6f
Charan Kumar H <131192607+CharanKumarH@users.noreply.github.com>
2025-12-14 23:52:46 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 set -e
+docker stop $(docker ps -q)
 
-LATEST_CONTAINER_ID=$(docker ps -q | head -n 1)
-
-if [ -n "$LATEST_CONTAINER_ID" ]; then
-  docker rm -f "$LATEST_CONTAINER_ID"
-fi
 
 echo "Hi -----"
