cf3a777480523ae07a5123b0f602bc80b5bf7513
Charan Kumar H <131192607+CharanKumarH@users.noreply.github.com>
2025-12-14 23:43:42 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Stop the running container (if any)
-latest_container_id =$(docker ps | awk -F " " '{print $1}')
+latest_container_id =`docker ps | awk -F " " '{print $1}'`
 docker rm -f latest_container_id
 echo "Hi"
