5f98a76414a263a492e50c38104d2e82c776a097
Charan Kumar H <131192607+CharanKumarH@users.noreply.github.com>
2025-12-15 00:00:33 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker pull ram1to6/simple-flask-application
+CONTAINER_NAME=flask-app
+IMAGE=ram1to6/simple-flask-application:latest
 
-# Run the Docker image as a container
-docker run -d -p 5000:5000 ram1to6/simple-flask-application
+# Stop & remove old container (SAFE)
+docker stop $CONTAINER_NAME 2>/dev/null || true
+docker rm   $CONTAINER_NAME 2>/dev/null || true
+
+# Pull latest image
+docker pull $IMAGE
+
+# Run new container
+docker run -d \
+  --name $CONTAINER_NAME \
+  -p 5000:5000 \
+  --restart always \
+  $IMAGE
+
+echo "Container started successfully"
